ec3ac16a2e511e5e8738c087de27e06acd60763f
Hannah Collins <hannahcollins53@gmail.com>
2023-03-15 14:09:11 -0700

    almost done with messaging

--- src/messages/MessageDetails.js
+++ src/messages/MessageDetails.js
@@ -1,11 +1,18 @@
-import { getUserById } from "../auth/LoginProvider"
+import {  useNavigate } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { getUserProfileById } from "../auth/LoginProvider"
+import { useRef } from 'react';
+import { AddNewMessage } from "./MessageProvider";
+import { getUserSentMessages } from "./MessageProvider";
+import { getUserReceivedMessages } from "./MessageProvider";
 
-export const MessageDetails = ({fullMessages, userId}) => {
+export const MessageDetails = ({fullMessages, userId, updateReceivedMessages, updateSentMessages}) => {
     const[userProfile, updateUser] = useState({})
     const[isLoading, setIsLoading] = useState(true)
     const[myMessages, setMyMessages]= useState([])
+    const localHiker = localStorage.getItem("hike_user")
+    const hikeUser = JSON.parse(localHiker)
+    const navigate = useNavigate()
     useEffect(
         () => {
            if(userId) {
@@ -23,28 +30,52 @@ export const MessageDetails = ({fullMessages, userId}) => {
     )
     const getMessages = () => {
         if(fullMessages.length)
-       { let messages = fullMessages.map(fullMessage => fullMessage.filter(message => (message.receiverId === parseInt(userId) || message.senderId === parseInt(userId)))).filter(fullMessage => fullMessage.length)
+       { let messages = fullMessages.filter(message => message.senderId === parseInt(userId) || message.receiverId === parseInt(userId))
         setMyMessages(messages)}
     }
     const SortMessages = () => {
-       return myMessages[0]?.map(myMessage =>  {
-         if(myMessage.senderId === parseInt(userId)) {
-           return <div className="chat chat-start">
-            <div className="mb-2 chat-bubble"> {myMessage.body} </div> <div className="chat-footer opacity-50">
-            Delivered
-            </div></div>
-        }     
-        else {
-            return <div className="chat chat-end">
-            <div className="mb-2 chat-bubble"> {myMessage.body} </div> <div className="chat-footer opacity-50">
-            Delivered
-            </div></div>
-        }} )
+       return myMessages?.map(message =>{
+            if(message.senderId === parseInt(userId)) {
+              return <div className="chat chat-start">
+               <div className="mb-2 chat-bubble"> {message.body} </div> <div className="chat-footer opacity-50">
+               Delivered
+               </div></div>
+           }     
+           else {
+               return <div className="chat chat-end">
+               <div className="mb-2 chat-bubble"> {message.body} </div> <div className="chat-footer opacity-50">
+               Delivered
+               </div></div>
+           }} )
+        
     }
+    const inputRef = useRef(null)
+
+   const handleSendButtonClick = (event) => {
+    event.preventDefault()
+    const newMessage = {
+        senderId: hikeUser.id,
+        receiverId: parseInt(userId),
+        body: inputRef.current.value,
+        date: new Date().toLocaleString()
+    }
+    AddNewMessage(newMessage).then(
+        getUserSentMessages(hikeUser).then((sentMessagesArray) => {
+            updateSentMessages(sentMessagesArray)
+         }).then(
+        getUserReceivedMessages(hikeUser).then((receivedMessagesArray) => {
+            updateReceivedMessages(receivedMessagesArray)
+        })).then(
+            getMessages()
+        )
+   
+    )
+}
+   
     return <> 
-    <div className="ml-6 p-4 flex flex-col h-96 w-11/12 border-2 border-slate-500 bg-silver">
-    <div className="h-100">
-    <div className="flex flex-col h-72">
+    <div className="ml-6 p-4 flex flex-col h-96 w-11/12 border-2 border-slate-500 bg-silver h-full">
+    <div className="h-full">
+    <div className="flex flex-col">
         <div className="flex flex-row items-center justify-center mb-4">
             {
                 userProfile ? <img src={userProfile[0]?.image} className="h-[25px] rounded-full mr-4" />
@@ -68,10 +99,14 @@ export const MessageDetails = ({fullMessages, userId}) => {
     </section>
     </div>
     </div>
-   <div>
+   <div className="flex justify-center">
     <input type="text" placeholder="Type a message"
-    className="rounded-lg  placeholder:text-sm "
+    ref={inputRef}
+    className="rounded-lg  placeholder:text-sm mr-4"
     />
+    <button className="btn btn-sm btn-lightSand" 
+    onClick={(clickEvent) => handleSendButtonClick(clickEvent)}
+    >send</button>
    </div>
     </div>
     </>
